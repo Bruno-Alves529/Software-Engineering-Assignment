e9eeeadcf9ba9ff39daab320b45b9c9869c657c3
AndriusAl <45125947+AndriusAl@users.noreply.github.com>
2019-11-08 09:42:57 +0000

    Add files via upload
    This is my newest GUI. 07/11/2019

---
+++ GUI3/C#/Login/Login/Form2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void monitoringPatientToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void johnSmitthToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Form3().Show();
+            this.Hide();
+        }
+
+        private void smithJohnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void andySmithToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+            
+           
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Form3().Show();
+            this.Hide();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GUI3/C#/Login/bin/Debug/Form1.cs
@@ -0,0 +1,89 @@
+﻿//Full code for  login taken from this website:
+//https://code.msdn.microsoft.com/windowsapps/Login-Form-using-Windows-a2bd41b4
+//Modified only design
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+           
+        }
+        //Enter code here for your version of username and userpassword
+        Login login = new Login("admin", "1234");
+       
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //define local variables from the user inputs
+            string user = nametxtbox.Text;
+            string pass = pwdtxtbox.Text;
+            //check if eligible to be logged in
+            if(login.IsLoggedIn(user,pass))
+            {
+                MessageBox.Show("You are logged in successfully");
+                new Form2().Show();
+                this.Hide();
+            }
+            else
+            {
+                //show default login error message
+                MessageBox.Show("Login Error!");
+            }
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //enter your code for forget password case
+            MessageBox.Show("Under development");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //Enter your code for registration form of your choice
+            MessageBox.Show("Under development");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nametxtbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pwdtxtbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
